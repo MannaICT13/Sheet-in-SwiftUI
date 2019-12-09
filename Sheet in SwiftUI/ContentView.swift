586cc31c3972956d4bf17190b8131a4ce0f513f7
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2019-12-10 00:51:21 +0600

    Added a ContentView2

--- Sheet in SwiftUI/ContentView.swift
+++ Sheet in SwiftUI/ContentView.swift
@@ -8,11 +8,25 @@
 
 import SwiftUI
 
+
+
+struct ContentView2 : View{
+    
+    var body : some View{
+        Text("Hello World")
+    }
+}
+
+
 struct ContentView: View {
     var body: some View {
         Text("Hello World")
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
